dcd69e68c11dc7e998acc47094867a369be8948c
gomez50057 <gomez.50057@gmail.com>
2024-08-26 14:52:44 -0600

    models de forms

--- Django/forms/models.py
+++ Django/forms/models.py
@@ -13,7 +13,11 @@ class Acuerdo(models.Model):
     fecha_creacion = models.DateField(auto_now_add=True)
     estatus = models.CharField(max_length=50, choices=ESTATUS_CHOICES, default='en_proceso')
     acuerdo_original = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='versiones')
-    
+
+    # Campos nuevos para Estado y Comisión
+    estado = models.CharField(max_length=100, blank=True, null=True)
+    comision = models.CharField(max_length=100, blank=True, null=True)
+
     nombre = models.CharField(max_length=100)
     apellido_paterno = models.CharField(max_length=100)
     apellido_materno = models.CharField(max_length=100)
@@ -28,22 +32,29 @@ class Acuerdo(models.Model):
     def save(self, *args, **kwargs):
         if not self.id_unico:
             current_year = datetime.date.today().year
-            commission_siglas = "SC"  # Aquí deberías obtener dinámicamente las siglas de la comisión
+            commission_siglas = self.comision[:2].upper() if self.comision else "SC"  # Obtener siglas de la comisión de forma dinámica
             total_acuerdos = Acuerdo.objects.filter(fecha_creacion__year=current_year).count() + 1
 
-            self.id_unico = f"AC{current_year}-{commission_siglas}-{total_acuerdos:03d}"
-        
-        super(Acuerdo, self).save(*args, **kwargs)
+            # Evitar duplicados incrementando hasta encontrar un ID único
+            potential_id_unico = f"AC{current_year}-{commission_siglas}-{total_acuerdos:03d}"
+            while Acuerdo.objects.filter(id_unico=potential_id_unico).exists():
+                total_acuerdos += 1
+                potential_id_unico = f"AC{current_year}-{commission_siglas}-{total_acuerdos:03d}"
 
-    def __str__(self):
-        return f'{self.id_unico} - {self.nombre} {self.apellido_paterno}'
+            self.id_unico = potential_id_unico
+
+        super(Acuerdo, self).save(*args, **kwargs)
 
 class Actualizacion(models.Model):
     acuerdo = models.ForeignKey(Acuerdo, on_delete=models.CASCADE, related_name='actualizaciones')
     fecha_actualizacion = models.DateField(auto_now_add=True)
     descripcion_avance = models.TextField(max_length=5000)
     documentos = models.FileField(upload_to='documentos/', blank=True, null=True)
-    version = models.IntegerField(editable=False)  # Marcamos como no editable
+    version = models.IntegerField(editable=False)
+
+    # Campos duplicados del acuerdo para simplificar la creación de la actualización
+    estado = models.CharField(max_length=100, blank=True, null=True)
+    comision = models.CharField(max_length=100, blank=True, null=True)
 
     nombre = models.CharField(max_length=100)
     apellido_paterno = models.CharField(max_length=100)
@@ -54,8 +65,12 @@ class Actualizacion(models.Model):
     correo = models.EmailField()
 
     def save(self, *args, **kwargs):
+        # Asignar estado y comisión automáticamente del acuerdo original
+        if self.acuerdo:
+            self.estado = self.acuerdo.estado
+            self.comision = self.acuerdo.comision
+
         if not self.version:
-            # Calculamos la versión en función del número de actualizaciones existentes para este acuerdo
             self.version = self.acuerdo.actualizaciones.count() + 1
         
         super(Actualizacion, self).save(*args, **kwargs)
